c917e83f6da77cc957fffa2071e601cf9ceed66c
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-08-01 14:06:05 +0600

    update nav bar

--- view/layouts/app.php
+++ view/layouts/app.php
@@ -1,9 +1,12 @@
 <?php 
 include_once  __DIR__ . DIRECTORY_SEPARATOR . 'header.php';
+include_once  __DIR__ . DIRECTORY_SEPARATOR . '../header_nav.php';
 include_once  __DIR__ . DIRECTORY_SEPARATOR . '../home.php';
 include_once  __DIR__ . DIRECTORY_SEPARATOR . '../about.php';
 include_once  __DIR__ . DIRECTORY_SEPARATOR . '../skills.php';
 include_once  __DIR__ . DIRECTORY_SEPARATOR . '../work.php';
-include_once  __DIR__ . DIRECTORY_SEPARATOR . '../contact.php';
+include_once  __DIR__ . DIRECTORY_SEPARATOR . '../blog.php';
+
+// include_once  __DIR__ . DIRECTORY_SEPARATOR . '../contact.php';
 include_once  __DIR__ . DIRECTORY_SEPARATOR . 'footer.php';
 ?>
