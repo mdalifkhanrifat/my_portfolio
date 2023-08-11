628063d4cf57e3bc4df10ebf057d263bce7d989b
mdalifkhanrifat <mdalifkhanrifat@gmail.com>
2023-08-12 00:46:15 +0600

    error test

--- view/work.php
+++ view/work.php
@@ -3,19 +3,16 @@
             <h2 class="section-title">Work</h2>
 
             <div class="work__container bd-grid">
-                <a href="" class="work__img">
+                <a href="https://github.com/mdalifkhanrifat/income_tax" class="work__img">
                     <img src="assets/img/work1.jpg" alt="">
                 </a>
-                <a href="" class="work__img">
-                    <img src="assets/img/work2.jpg" alt="">
-                </a>
-                <a href="" class="work__img">
+                <a href="https://drive.google.com/file/d/10WSHVQPtPoJBapWUEeWbUb_EJrlLVr2U/view" class="work__img">
                     <img src="assets/img/work3.jpg" alt="">
                 </a>
-                <a href="" class="work__img">
-                    <img src="assets/img/work4.jpg" alt="">
+                <a href="https://github.com/Mohib04/Apartment-Management-System" class="work__img">
+                    <img src="assets/img/work4.png" alt="">
                 </a>
-                <a href="" class="work__img">
+                <a href="https://github.com/mdalifkhanrifat/phone_book" class="work__img">
                     <img src="assets/img/work5.jpg" alt="">
                 </a>
                 <!-- <a href="" class="work__img">
